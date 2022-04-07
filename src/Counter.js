2b04ea66047e5f19fbb370c5250423aa22d1c89d
wildfirez98 <james@areafamily.us>
2022-04-06 21:32:13 -0500

    First commit, everything working!

---
+++ src/Counter.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./features/counterSlice";
+
+const Counter = () => {
+    const count = useSelector((state) => state.value)
+    const dispatch = useDispatch()
+    const [ input, setInput ] = useState(0)
+    const byAmountSubmit = (e) => {
+        e.preventDefault()
+        dispatch(incrementByAmount(Number(input)))
+    }
+
+    return(
+        <div>
+            <h1>
+                {count}
+            </h1>
+            <button 
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}>
+                Increment
+            </button>
+            <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}>
+                Decrement
+            </button>
+            <form onSubmit={(e) => byAmountSubmit(e)}>
+                <input type="number" onChange={(e) => setInput(e.target.value)} />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Counter
